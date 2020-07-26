3bf28ef94994fe3966f8b4cbc35b47d234a46060
Sai1919 <51651413+Saikamesh@users.noreply.github.com>
2020-07-26 18:44:31 +0530

    Completely Functioning Tic-Tac-Toe

--- src/index.js
+++ src/index.js
@@ -1,17 +1,88 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React,{useState} from 'react';
+import ReactDom from 'react-dom';
+
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const Square = (props) => {
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+    return <button className='square' onClick={props.onclickEvent}>
+      {props.value}
+  </button>;
+};
+
+const Board = () => {
+
+    const initialSquares = Array(9).fill(null);
+    const[squares,setSquares] = useState(initialSquares);
+    const [xIsNext, setXIsNext]=useState(true);
+
+    const handleClickEvent=(i)=>{
+            const newSquares = [...squares];
+            const winnerDeclared = Boolean(calculateWinner(newSquares));
+            const squareFilled = Boolean(newSquares[i]);
+            if(winnerDeclared || squareFilled){
+                return;
+            }
+            newSquares[i]=xIsNext?'X':'O';
+            setSquares(newSquares);
+            setXIsNext(!xIsNext);
+    };
+
+  const renderSquare = (i) => {
+    return (<Square value={squares[i]}
+    onclickEvent={()=>handleClickEvent(i)} />);
+  };
+    const winner = calculateWinner(squares);
+
+  const status = winner ? `Winner: ${winner}` : `Next Player: ${xIsNext ? 'X': 'O'} `;
+
+  return (
+    <div className='status'>
+      <div>{status}</div>
+      <div className='board-row'>
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className='board-row'>
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className='board-row'>
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
+};
+
+const Game = () => {
+  return (
+    <div className='game'>
+        Tic-Tac-Toe
+      <Board />
+    </div>
+  );
+};
+
+ReactDom.render(<Game />, document.getElementById('root'));
+
+function calculateWinner(squares) {
+    const lines=[
+        [0,1,2],[3,4,5],[6,7,8], //rows
+        [0,3,6],[1,4,7],[2,5,8], //columns
+        [0,4,8],[2,4,6], //diagonals
+    ];
+
+    for(let line of lines){
+        const[a,b,c]=line;
+
+        if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+            return squares[a]; //X or O
+        }
+    }
+    return null;
+}
